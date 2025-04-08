f6627e833420c14df3c57a8420e9405d4f4620fb
kreon <kreonike@gmail.com>
2025-04-08 22:29:43 +0300

    08.04.2025 bot 1

--- bot/config/constants.py
+++ bot/config/constants.py
@@ -5,3 +5,36 @@ MONTH_NAMES = {
 }
 
 REMINDER_INTERVAL = 30  # seconds
+
+# SQL-запросы для напоминаний
+QUERY_24H = '''
+    SELECT t.id, t.task, t.time, tu.telegram_id
+    FROM tasks t
+    JOIN telegram_users tu ON t.user_id = tu.user_id
+    WHERE t.reminder_1day_sent = 0
+    AND t.year = ? AND t.month = ? AND t.day = ?
+    AND t.time IS NOT NULL
+'''
+
+QUERY_2H = '''
+    SELECT t.id, t.task, t.time, tu.telegram_id
+    FROM tasks t
+    JOIN telegram_users tu ON t.user_id = tu.user_id
+    WHERE t.reminder_2h_sent = 0
+    AND t.year = ? AND t.month = ? AND t.day = ?
+    AND t.time IS NOT NULL
+'''
+
+QUERY_15M = '''
+    SELECT t.id, t.task, t.time, tu.telegram_id
+    FROM tasks t
+    JOIN telegram_users tu ON t.user_id = tu.user_id
+    WHERE t.reminder_15m_sent = 0
+    AND t.year = ? AND t.month = ? AND t.day = ?
+    AND t.time IS NOT NULL
+'''
+
+UPDATE_REMINDER_STATUS = '''
+    UPDATE tasks SET {} = 1 
+    WHERE id = ?
+'''
