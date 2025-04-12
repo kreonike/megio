9f91ba7c0f6b0f4212cd717bd69c2379b7af9525
kreon <kreonike@gmail.com>
2025-04-12 17:34:25 +0300

    12.04.2025 2

--- web/services/category_service.py
+++
@@ -1,4 +0,0 @@
-def get_user_categories(db, user_id):
-    cursor = db.cursor()
-    cursor.execute('SELECT id, name, color FROM categories WHERE user_id = ?', (user_id,))
-    return [dict(row) for row in cursor.fetchall()]
--- web/utils.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify
-
-def json_response(success=True, data=None, error=None, status_code=200):
-    response = {'success': success}
-    if data is not None:
-        response['data'] = data
-    if error is not None:
-        response['error'] = error
-    return jsonify(response), status_code
